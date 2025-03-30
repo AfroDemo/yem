92a0ac021c4417c1e79cd3eb61e43c2783fd2f61
afrodemo <hasaniymkindi@gmail.com>
2025-03-30 15:22:54 +0300

    Add events migration with detailed schema and table creation

--- back/migrations/20250330121657-create-events.js
+++ back/migrations/20250330121657-create-events.js
@@ -1,22 +1,88 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("events", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM("workshop", "networking", "webinar", "conference"),
+        allowNull: false,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.JSON, 
+        defaultValue: {},
+      },
+      host: {
+        type: Sequelize.INTEGER, 
+        references: {
+          model: "users",
+          key: "id",
+        },
+        allowNull: false,
+      },
+      speakers: {
+        type: Sequelize.JSON, 
+        defaultValue: [],
+      },
+      maxAttendees: {
+        type: Sequelize.INTEGER,
+      },
+      currentAttendees: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      registrationLink: {
+        type: Sequelize.STRING,
+      },
+      registrationDeadline: {
+        type: Sequelize.DATE,
+      },
+      registrationFee: {
+        type: Sequelize.DECIMAL,
+      },
+      registrationCurrency: {
+        type: Sequelize.STRING,
+      },
+      thumbnail: {
+        type: Sequelize.STRING,
+      },
+      tags: {
+        type: Sequelize.JSON,
+        defaultValue: [],
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("events");
+  },
 };
